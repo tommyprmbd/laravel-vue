6ef11bea21f26ca890442de9ca5af19aecf6f7a1
asmeldipostman <asmeldipostman@gmail.com>
2022-10-23 15:09:31 +0700

    tugas php operator

---
+++ Asmeldi/Tugas_PHP/bangun_datar.php
@@ -0,0 +1,55 @@
+<?php
+
+//luas persegi panjang
+$l = 20;
+$p = 40;
+$lpp = $l * $p;
+echo "<b>Rumus Persegi Panjang</b><br><br>";
+echo "Lebar = $l <br>";
+echo "Panjang = $p <br>";
+echo "Luas Persegi Panjang <br>";
+echo "$l * $p = $lpp";
+echo "<hr>";
+
+//luas Persegi
+$s = 10;
+$lp = $s ** 2;
+echo "<b>Rumus Persegi</b><br><br>";
+echo "Sisi = $s <br>";
+echo "Luas Persegi <br>";
+echo "$s * $s = $lp";
+echo "<hr>";
+
+//Luas Bangun Jajar genjang
+$a = 10;
+$t = 30;
+$jg = $a * $t;
+echo "<b>Rumus Luas Bangun Jajar genjang</b><br><br>";
+echo "alas = $a <br>";
+echo "tinggi = $t <br>";
+echo "Luas Bangun Jajar genjang <br>";
+echo "$a * $t = $jg";
+echo "<hr>";
+
+
+//luas Lingkaran
+$r = 4;
+$ll = 3.14 * $r ** 2;
+echo "<b>Rumus Lingkaran</b><br><br>";
+echo "Jari-jari = $r <br>";
+echo "Luas Lingkaran <br>";
+echo "3.14 * $r * $r = $ll";
+echo "<hr>";
+
+//luas Trapesium
+$at = 15;
+$b = 8;
+$tt = 10;
+$lt = (($at + $b) * $tt) / 2;
+echo "<b>Rumus Trapesium</b><br><br>";
+echo "Sisi atas = $at <br>";
+echo "Sisi bawah = $b <br>";
+echo "Tinggi = $tt <br>";
+echo "Luas Trapesium <br>";
+echo "(($at + $b) * $tt)/2 = $lt";
+echo "<hr>";
