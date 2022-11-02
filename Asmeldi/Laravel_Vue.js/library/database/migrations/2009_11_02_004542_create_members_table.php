077d905adf36081be05acb305f6200d934a4cd6f
asmeldipostman <asmeldipostman@gmail.com>
2022-11-02 08:11:42 +0700

    Membuat model dan migration

---
+++ Asmeldi/Laravel_Vue.js/library/database/migrations/2009_11_02_004542_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->char('gender', 1);
+            $table->char('phone_number', 12);
+            $table->text('adress');
+            $table->string('email', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
