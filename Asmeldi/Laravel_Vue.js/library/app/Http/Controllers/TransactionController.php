f89c092ce8c9cfffebca783628c2aa22b91ed13c
asmeldipostman <asmeldipostman@gmail.com>
2022-11-02 09:10:05 +0700

    membuat controller dari terminal

---
+++ Asmeldi/Laravel_Vue.js/library/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(transaction $transaction)
+    {
+        //
+    }
+}
