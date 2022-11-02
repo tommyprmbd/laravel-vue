e3a49470b0e8f5a2a6a7ac8193c130969a9eb1f7
asmeldipostman <asmeldipostman@gmail.com>
2022-11-02 08:55:30 +0700

    update assigment add MemberSeeder

---
+++ Asmeldi/Laravel_Vue.js/library/database/seeders/MemberSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Members;
+use Faker\Factory as Faker;
+
+use Illuminate\Database\Seeder;
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 200; $i++) {
+            $Members = new Members();
+
+            $Members->name = $faker->name;
+            $Members->gender = $faker->randomElement(['l', 'p']);
+            $Members->phone_number = '0813' . $faker->randomNumber(8);
+            $Members->adress = $faker->address;
+            $Members->email = $faker->email;
+
+
+            $Members->save();
+        }
+    }
+}
