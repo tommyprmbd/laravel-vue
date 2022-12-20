82c76f5defa9105752e5ae8c847938c58cc903a4
budie300 <sitisukagoyang@gmail.com>
2022-12-20 20:35:40 +0700

    koreksi tugas

--- Budi/tugas_php/function_ruang_datar.php
+++ Budi/tugas_php/function_ruang_datar.php
@@ -7,12 +7,14 @@
 	luas_persegi_panjang(8, 4);
 	echo "<hr>";
 
-	function luas_persegi($s, $s){
-		$Lp = $s * $s;
-		echo "$s * $s = $Lp";
+
+	function luas_persegi($Spi, $Si){
+		$Lp = $Spi * $Si;
+		echo "$Spi * $Si = $Lp";
 	}
 	luas_persegi(5, 5);
 	echo "<hr>";
+
 
 	function luas_jajar_genjang($a, $t){
 		$Ljg = $a * $t;
@@ -21,16 +23,18 @@
 	luas_jajar_genjang(9, 6);
 	echo "<hr>";
 
-	function volume_bangun_kubus($s, $s, $s){
-		$vk = $s * $s * $s;
-		echo "$s * $s * $s = $vk";
+
+	function volume_bangun_kubus($Sa, $Sb, $Sc){
+		$vk = $Sa * $Sb * $Sc;
+		echo "$Sa * $Sb * $Sc = $vk";
 	}
 	volume_bangun_kubus(4, 4, 4);
 	echo "<hr>";
 
-	function volume_bangun_balok($p, $l, $t){
-		$vbb = $p * $l * $t;
-		echo "$p * $t * $l = $vbb";
+
+	function volume_bangun_balok($pb, $lb, $tb){
+		$vbb = $pb * $lb * $tb;
+		echo "$pb * $lb * $tb = $vbb";
 	}
 	volume_bangun_balok(8, 5, 5);
 	echo "<hr>";
