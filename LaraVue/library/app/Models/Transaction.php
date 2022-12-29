8830fad1a292ed9cf1c0aa6938cae14b7aa5ff4d
tommypria <tommypriambodo@gmail.com>
2022-12-29 21:55:48 +0700

    mentor bantu buat contoh source data untuk datatable transaction

--- LaraVue/library/app/Models/Transaction.php
+++ LaraVue/library/app/Models/Transaction.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class, 'member_id');
+    }
+
+    public function transactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class, 'transaction_id');
+    }
+
+    public function getLamaPinjamAttribute()
+    {
+        $tglPinjam = Carbon::parse($this->date_start);
+        $tglKembali = Carbon::parse($this->date_end);
+        return $tglPinjam->diffInDays($tglKembali);
+    }
 }
--- LaraVue/library/app/Models/TransactionDetail.php
+++ LaraVue/library/app/Models/TransactionDetail.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionDetail extends Model
 {
     use HasFactory;
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class, 'book_id');
+    }
 }
