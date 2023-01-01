f680b25a1702409be039f7a5bfc52ee999668e15
budie300 <sitisukagoyang@gmail.com>
2023-01-02 00:04:04 +0700

    tugas templating

--- Budi/laravel/perpus/resources/views/layouts/RegMenu.blade.php
+++ Budi/laravel/perpus/resources/views/layouts/RegMenu.blade.php
@@ -1,10 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Perpus | Log in page</title>
+  <title>Perpus | Registration Page</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -15,9 +14,9 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
 </head>
-<body class="hold-transition login-page">
-<!-- /.login-box -->
- @yield('content')
+<body class="hold-transition register-page">
+<!-- /.register-box -->
+@yield('content')
 
 <!-- jQuery -->
 <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
