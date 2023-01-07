197f4e45f63cbf3311384c3cc88bffef5510e0fc
budie300 <sitisukagoyang@gmail.com>
2023-01-07 20:31:06 +0700

    fix registerbladephp

--- Budi/laravel/library/resources/views/auth/register.blade.php
+++ Budi/laravel/library/resources/views/auth/register.blade.php
@@ -53,8 +53,9 @@
             </div>
           </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input id="password-confirm" name="password_confirmation" type="password" class="form-control" placeholder="Retype password" required autocomplete="new-password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
