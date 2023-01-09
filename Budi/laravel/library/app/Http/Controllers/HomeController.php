1c08554036d6d00b52f37bf2b44850e744d995be
budie300 <sitisukagoyang@gmail.com>
2023-01-09 21:51:52 +0700

    Eloquent Relationship Model

--- Budi/laravel/library/app/Http/Controllers/HomeController.php
+++ Budi/laravel/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use App\Models\Book;
+use App\Models\Publisher;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //$members = Member::with('user')->get();
+        //$books = Book::with('publisher')->get();
+        //$publishers = Publisher::with('books')->get();
+
+        //return $publishers;
         return view('home');
     }
 }
